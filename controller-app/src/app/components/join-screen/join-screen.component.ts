bc65acb099f985b8cefb4698eb7e860272fbe81d
Gursimran27 <singhgursimran165@gmail.com>
2026-01-03 14:46:30 +0530

    feat: Add JoinScreenComponent for joining rooms via code or query params, including mobile fullscreen and orientation lock.

--- controller-app/src/app/components/join-screen/join-screen.component.ts
+++ controller-app/src/app/components/join-screen/join-screen.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { ControllerService } from '../../services/controller.service';
 
 @Component({
@@ -10,12 +11,28 @@ import { ControllerService } from '../../services/controller.service';
   templateUrl: './join-screen.component.html',
   styleUrl: './join-screen.component.css'
 })
-export class JoinScreenComponent {
+export class JoinScreenComponent implements OnInit {
   roomCodeInput = '';
   errorMsg: string | null = null;
 
-  constructor(private controllerService: ControllerService) {
-      this.controllerService.error$.subscribe(err => this.errorMsg = err);
+  constructor(
+    private controllerService: ControllerService,
+    private route: ActivatedRoute
+  ) {
+    this.controllerService.error$.subscribe(err => this.errorMsg = err);
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const room = params['room'];
+      if (room) {
+        this.roomCodeInput = room.toUpperCase();
+        // Auto-join if code is complete
+        if (this.roomCodeInput.length === 6) {
+          this.joinRoom();
+        }
+      }
+    });
   }
 
   joinRoom() {
@@ -26,19 +43,19 @@ export class JoinScreenComponent {
   }
 
   handleMobileDisplay() {
-      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-      if (isMobile) {
-          try {
-            const elem = document.documentElement;
-            if (elem.requestFullscreen) {
-                elem.requestFullscreen().catch(err => console.log('Fullscreen failed:', err));
-            } else if ((elem as any).webkitRequestFullscreen) {
-                (elem as any).webkitRequestFullscreen();
-            }
-            if (screen.orientation && (screen.orientation as any).lock) {
-                (screen.orientation as any).lock('landscape').catch((err: any) => console.log('Orientation lock failed:', err));
-            }
-          } catch(e) { console.log(e); }
-      }
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (isMobile) {
+      try {
+        const elem = document.documentElement;
+        if (elem.requestFullscreen) {
+          elem.requestFullscreen().catch(err => console.log('Fullscreen failed:', err));
+        } else if ((elem as any).webkitRequestFullscreen) {
+          (elem as any).webkitRequestFullscreen();
+        }
+        if (screen.orientation && (screen.orientation as any).lock) {
+          (screen.orientation as any).lock('landscape').catch((err: any) => console.log('Orientation lock failed:', err));
+        }
+      } catch (e) { console.log(e); }
+    }
   }
 }
